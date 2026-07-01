bbda96bd858e930fbdb6fde6b36058ed7ba0cb5a
Dika Pramudya Nugraha <pramudyan196@gmail.com>
2026-07-01 20:01:46 +0700

    Add BilanganKuadrat class header file

---
+++ Problem II - AJS dan Bilangan kuadrat/BilanganKuadrat.hpp
@@ -0,0 +1,15 @@
+#ifndef BILANGAN_KUADRAT_HPP
+#define BILANGAN_KUADRAT_HPP
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+class BilanganKuadrat {
+public:
+    static long long hitungJumlahKuadrat(long long a, long long b);
+};
+
+#endif 

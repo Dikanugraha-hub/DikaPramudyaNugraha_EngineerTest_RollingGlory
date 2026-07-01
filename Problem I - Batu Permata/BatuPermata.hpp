87ebe90f84f5148a2788704435112bdef5cb3e0c
Dika Pramudya Nugraha <pramudyan196@gmail.com>
2026-07-01 20:00:10 +0700

    Add BatuPermata class header file

---
+++ Problem I - Batu Permata/BatuPermata.hpp
@@ -0,0 +1,15 @@
+#ifndef BATU_PERMATA_HPP
+#define BATU_PERMATA_HPP
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class BatuPermata {
+    public:
+        static int hitungTotalPermata(int n, const vector<std::string>& kumpulanBatu);
+};
+
+#endif
